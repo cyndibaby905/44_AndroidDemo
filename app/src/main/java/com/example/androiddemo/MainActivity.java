7acd34fda4ff125dd88c38f85bcb32765ad7afdc
Hang Chen <hangisnice@gmail.com>
2019-09-27 14:18:59 +0800

    .gitignore fix

--- app/src/main/java/com/example/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/androiddemo/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         View FlutterView = Flutter.createView(this, getLifecycle(), "defaultRoute"); //传入路由标识符
         setContentView(FlutterView);//用FlutterView替代Activity的ContentView
+
     }
 }
